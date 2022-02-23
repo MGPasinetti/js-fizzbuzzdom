ba08258db510c0b64d7cf628b3f6a47dab7fd5a0
MGPasinetti <mg.pasinetti@gmail.com>
2022-02-23 17:47:17 +0100

    milestone 2 done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 // Consegna:
-// Scrivi un programma che stampi in console i numeri da 1 a 100.
-// MILESTONE 1
-// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// MILESTONE 2
+
 // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 // MILESTONE 3
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
@@ -15,10 +11,11 @@
 // Buon lavoro e buon divertimento!
 
 
-//Define listNumber
+//Variabili
 let listNumber;
+let container = document.querySelector(".container")
 
-//Generate numers from 1 to 100
+// Genero i numeri da 1 a 100
 for (i = 1; i <= 100; i++) {
   listNumber = i;
 
@@ -34,3 +31,28 @@ for (i = 1; i <= 100; i++) {
   }
 
 }
+
+//Appendo un elemento html con il numero o la stringa corretta
+let areaNumbers = document.createElement(`div`)
+container.append(areaNumbers)
+
+for (i = 1; i <= 100; i++) {
+    listNumber = i;
+  
+  
+    if ((listNumber % 3 == 0) && (listNumber % 5 == 0)) {
+      areaNumbers.innerHTML += `<div class="box fizz-buzz">${`FizzBuzz`}</div>`
+    } else if (listNumber % 3 == 0) {
+      areaNumbers.innerHTML += `<div class="box fizz">${`Fizz`}</div>`
+    } else if (listNumber % 5 == 0) {
+      areaNumbers.innerHTML += `<div class="box buzz">${`Buzz`}</div>`
+    } else {
+      areaNumbers.innerHTML += `<div class="box">${listNumber}</div>`
+    }
+  
+}
+
+
+
+
+
