6deb3414a45b88035042ecf4d1e33797f73402b5
MGPasinetti <mg.pasinetti@gmail.com>
2022-02-23 18:14:24 +0100

    milestone 3 done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 // Consegna:
 
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 // MILESTONE 3
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?
 // Consigli del giorno:
 // 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
@@ -13,7 +10,6 @@
 
 //Variabili
 let listNumber;
-let container = document.querySelector(".container")
 
 // Genero i numeri da 1 a 100
 for (i = 1; i <= 100; i++) {
@@ -33,6 +29,7 @@ for (i = 1; i <= 100; i++) {
 }
 
 //Appendo un elemento html con il numero o la stringa corretta
+let container = document.querySelector(".container")
 let areaNumbers = document.createElement(`div`)
 container.append(areaNumbers)
 
@@ -41,11 +38,11 @@ for (i = 1; i <= 100; i++) {
   
   
     if ((listNumber % 3 == 0) && (listNumber % 5 == 0)) {
-      areaNumbers.innerHTML += `<div class="box fizz-buzz">${`FizzBuzz`}</div>`
+      areaNumbers.innerHTML += `<div class="box fizz-buzz">${`fizzbuzz`}</div>`
     } else if (listNumber % 3 == 0) {
-      areaNumbers.innerHTML += `<div class="box fizz">${`Fizz`}</div>`
+      areaNumbers.innerHTML += `<div class="box fizz">${`fizz`}</div>`
     } else if (listNumber % 5 == 0) {
-      areaNumbers.innerHTML += `<div class="box buzz">${`Buzz`}</div>`
+      areaNumbers.innerHTML += `<div class="box buzz">${`buzz`}</div>`
     } else {
       areaNumbers.innerHTML += `<div class="box">${listNumber}</div>`
     }
