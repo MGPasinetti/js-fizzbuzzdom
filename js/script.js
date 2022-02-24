4e00dcaab698f7893446deaa93835bbe53f37b5d
MGPasinetti <mg.pasinetti@gmail.com>
2022-02-24 15:37:56 +0100

    === fixed

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ for (i = 1; i <= 100; i++) {
   listNumber = i;
 
 
-  if ((listNumber % 3 == 0) && (listNumber % 5 == 0)) {
+  if ((listNumber % 3 === 0) && (listNumber % 5 === 0)) {
     console.log('FizzBuzz');
-  } else if (listNumber % 3 == 0) {
+  } else if (listNumber % 3 === 0) {
     console.log('Fizz');
-  } else if (listNumber % 5 == 0) {
+  } else if (listNumber % 5 === 0) {
     console.log('Buzz');
   } else {
     console.log(listNumber);
@@ -27,11 +27,11 @@ for (i = 1; i <= 100; i++) {
     listNumber = i;
   
   
-    if ((listNumber % 3 == 0) && (listNumber % 5 == 0)) {
+    if ((listNumber % 3 === 0) && (listNumber % 5 === 0)) {
       areaNumbers.innerHTML += `<div class="box fizz-buzz">fizzbuzz</div>`
-    } else if (listNumber % 3 == 0) {
+    } else if (listNumber % 3 === 0) {
       areaNumbers.innerHTML += `<div class="box fizz">fizz</div>`
-    } else if (listNumber % 5 == 0) {
+    } else if (listNumber % 5 === 0) {
       areaNumbers.innerHTML += `<div class="box buzz">buzz</div>`
     } else {
       areaNumbers.innerHTML += `<div class="box">${listNumber}</div>`
